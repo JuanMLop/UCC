e0fe196864b800f519e8d8675fecbb29b18a7cf6
JuanLop <juancamilo3ad2011@gmail.com>
2024-02-22 12:19:32 -0500

    Metodos

--- Empleado/Empleado.py
+++ Empleado/Empleado.py
@@ -1,9 +1,37 @@
 class Empleado:
-    #codigo
+    #Aqui va el codigo
+
+    '''----------------------------
+    # Atributos
+    ----------------------------'''
     nombre=""  
     apellido = ""
+
     '''----------------------------
     # 1= Masculino y 2= Femenino
     ----------------------------'''
-    sexo= 0
-    salario= 0
+    sexo= ""
+    salario= ""
+
+    '''----------------------------
+    # Metodos
+    ----------------------------'''
+    def CambiarSalario(self, nuevoSalario):
+        # Aqui va el codigo del medoto
+        return 0
+    def CambiarEmpleado(self, nNombre, nApellido, nSexo, nSalrio):
+        # Aqui va el codigo del medoto
+        return  None
+    def ConsultarSalario(self):
+        # Aqui va el codigo del medoto
+        return self.salario
+    def ConsultarNombre(self):
+        # Aqui va el codigo del medoto
+        return self.nombre
+    def ConsultarApellido(self):
+        # Aqui va el codigo del medoto
+        return self.apellido
+    def ConsultarNombreCompleto(self):
+        # Aqui va el codigo del medoto
+        return self.nombre + self.apellido
+    
